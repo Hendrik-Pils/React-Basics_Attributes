5e5f5a9302af25a8c719e28f0ae0d82f46ffe0fe
Hendrik Pils <hendrik-pils@gmx.de>
2023-12-01 12:39:06 +0100

    finish challenge

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article className="article">
+      <h2 className="article__title">HELLO WORLD</h2>
+      <label htmlFor="test">What do you like?</label>
+      <input type="text" name="test" id="test" />
+      <a href="http://www.wikipedia.de" className="aricle__link">
+        Link to Wikipedia
+      </a>
+    </article>
+  );
 }
